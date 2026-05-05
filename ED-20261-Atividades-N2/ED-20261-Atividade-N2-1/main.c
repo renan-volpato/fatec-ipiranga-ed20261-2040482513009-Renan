2dec9567d587d830c10e9d7437762e0581a7f42f
Renan Volpato <renanvolpato63@gmail.com>
2026-05-05 01:32:56 -0300

    Add main function for binary tree operations

---
+++ ED-20261-Atividades-N2/ED-20261-Atividade-N2-1/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "minhabib.h"
+
+int main() {
+    No* raiz = NULL;
+
+    int valores[] = {50, 30, 70, 20, 40, 60, 80};
+    for (int i = 0; i < 7; i++)
+        raiz = inserir(raiz, valores[i]);
+
+    analisar_arvore(raiz, 30);
+
+    liberar_arvore(raiz);
+  
+    return 0;
+}
