813a2f5a350303da9429cd780187e2dcbe4caaa6
Renan Volpato <renanvolpato63@gmail.com>
2026-05-05 01:28:47 -0300

    Add header file for binary search tree operations

---
+++ ED-20261-Atividades-N2/ED-20261-Atividade-N2-1/minhabib.h
@@ -0,0 +1,30 @@
+#ifndef MINHABIB_H
+#define MINHABIB_H
+
+typedef struct No {
+    int valor;
+    struct No *esq;
+    struct No *dir;
+} No;
+
+// Função principal de diagnóstico da atividade
+void analisar_arvore(No* raiz, int valorBusca);
+
+// Funções auxiliares obrigatórias para organização
+void imprimir_nos_internos(No* raiz);
+void imprimir_folhas(No* raiz);
+void imprimir_niveis(No* raiz, int nivel_atual);
+int calcular_altura(No* no);
+int calcular_profundidade(No* raiz, int valor, int profundidade_atual);
+void imprimir_ancestrais(No* raiz, int valor);
+void imprimir_descendentes(No* no);
+
+// Funções extras de apoio para a BST e para a demonstração
+No* criar_no(int valor);
+No* inserir(No* raiz, int valor);
+No* buscar_no(No* raiz, int valor);
+void imprimir_arvore_visual(No* raiz, int espaco);
+void liberar_arvore(No* raiz);
+int calcular_grau(No* no);
+
+#endif
