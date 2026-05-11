7da7b2a6e530b5b36f46a95ebf4c341d28357d81
Renan Volpato <renanvolpato63@gmail.com>
2026-05-11 14:48:22 -0300

    Add recursive functions for binary tree balancing
    Implement recursive logic for balancing binary search trees, including functions to calculate node height and balance factor.

---
+++ ED-20261-Atividades-N2/ED-20261-Atividade-N2-2/N2-2.c
@@ -0,0 +1,55 @@
+/* transição do cálculo manual para a lógica recursiva no balanceamento de Árvores Binárias de Busca, 
+   implementar funções em C que calculem a altura dos nós e o Fator de Balanceamento */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct No {
+    int valor;
+    struct No *esq;
+    struct No *dir;
+};
+
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+int calcularAltura(struct No* n) {
+    if (n == NULL) {
+        return -1;
+    }
+    return 1 + max(calcularAltura(n->esq), calcularAltura(n->dir));
+}
+
+int obterFB(struct No* n) {
+    if (n == NULL) return 0;
+    return calcularAltura(n->esq) - calcularAltura(n->dir);
+}
+
+struct No* novoNo(int valor) {
+    struct No* no = (struct No*)malloc(sizeof(struct No));
+    no->valor = valor;
+    no->esq = NULL;
+    no->dir = NULL;
+    return no;
+}
+
+int main() {
+    struct No* raiz = novoNo(20);
+    raiz->esq = novoNo(10);
+    raiz->esq->esq = novoNo(5);
+    raiz->esq->esq->esq = novoNo(2);
+
+    int fb = obterFB(raiz);
+
+    printf("Altura da Raiz: %d\n", calcularAltura(raiz));
+    printf("Fator de Balanceamento (FB) da Raiz: %d\n", fb);
+
+    if (fb > 1 || fb < -1) {
+        printf("Status: Desbalanceado! \n");
+    } else {
+        printf("Status: Balanceado.\n");
+    }
+
+    return 0;
+}
